b0616d522a9d02832b2f45895d338ff5e057e38b
Georgy <i@64mb.ru>
2026-01-17 03:20:20 +0300

    fix

--- lib/error.ts
+++ lib/error.ts
@@ -5,8 +5,12 @@ export const toYdbError = (error: unknown) => {
   if (ydbError?.issues?.[0]) {
     const ydbNestedError = ydbError.issues[0] as unknown as YdbErrorType
     if (ydbNestedError.issues?.[0]) {
-      const ydbNestedErrorMessage = (ydbError.issues[0] as unknown as YdbErrorType).message
-      return new Error(ydbNestedErrorMessage)
+      const ydbSuperNestedError = ydbNestedError.issues[0] as unknown as YdbErrorType
+      if (ydbSuperNestedError.issues?.[0]) {
+        const ydbNestedErrorMessage = (ydbSuperNestedError.issues[0] as unknown as YdbErrorType).message
+        return new Error(ydbNestedErrorMessage)
+      }
+      return new Error(ydbSuperNestedError.message)
     }
     return new Error(ydbNestedError.message)
   }
